7a72e9af1b550b38bbdc908dc24812744909d782
The17thDoctor <40616434+The17thDoctor@users.noreply.github.com>
2025-02-12 22:44:13 +0100

    Initial commit

---
+++ addon/lua/autorun/client/modbridge_v2_spawnmenu.lua
@@ -0,0 +1,208 @@
+ local size_1 = {
+    "models/modbridge_v2/corridors/size1/straight1.mdl",
+    "models/modbridge_v2/corridors/size1/straight2.mdl",
+    "models/modbridge_v2/corridors/size1/straight3.mdl",
+    "models/modbridge_v2/corridors/size1/turn.mdl",
+    "models/modbridge_v2/corridors/size1/3way.mdl",
+    "models/modbridge_v2/corridors/size1/4way.mdl",
+    "models/modbridge_v2/corridors/size1/deadend.mdl",
+    "models/modbridge_v2/corridors/size1/glassend.mdl",
+    "models/modbridge_v2/corridors/size1/glass_2p.mdl",
+    "models/modbridge_v2/corridors/size1/glass_1p.mdl",
+    "models/modbridge_v2/corridors/size1/glass_0p1.mdl",
+    "models/modbridge_v2/corridors/size1/glass_0p2.mdl",
+    "models/modbridge_v2/corridors/size1/glass_0p3.mdl",
+    "models/modbridge_v2/corridors/size1/straight1_sw.mdl",
+    "models/modbridge_v2/corridors/size1/straight2_sw.mdl",
+    "models/modbridge_v2/corridors/size1/straight3_sw.mdl",
+}
+
+local size_2 = {
+    "models/modbridge_v2/corridors/size2/straight1.mdl",
+    "models/modbridge_v2/corridors/size2/straight2.mdl",
+    "models/modbridge_v2/corridors/size2/straight3.mdl",
+    "models/modbridge_v2/corridors/size2/turn.mdl",
+    "models/modbridge_v2/corridors/size2/3way.mdl",
+    "models/modbridge_v2/corridors/size2/4way.mdl",
+    "models/modbridge_v2/corridors/size2/deadend.mdl",
+    "models/modbridge_v2/corridors/size2/glassend.mdl",
+    "models/modbridge_v2/corridors/size2/glass_2p.mdl",
+    "models/modbridge_v2/corridors/size2/glass_1p.mdl",
+    "models/modbridge_v2/corridors/size2/glass_0p1.mdl",
+    "models/modbridge_v2/corridors/size2/glass_0p2.mdl",
+    "models/modbridge_v2/corridors/size2/glass_0p3.mdl",
+    "models/modbridge_v2/corridors/size2/straight1_sw.mdl",
+    "models/modbridge_v2/corridors/size2/straight2_sw.mdl",
+    "models/modbridge_v2/corridors/size2/straight3_sw.mdl",
+}	
+
+local size_3 = {
+    "models/modbridge_v2/corridors/size3/straight1.mdl",
+    "models/modbridge_v2/corridors/size3/straight2.mdl",
+    "models/modbridge_v2/corridors/size3/straight3.mdl",
+    "models/modbridge_v2/corridors/size3/turn.mdl",
+    "models/modbridge_v2/corridors/size3/3way.mdl",
+    "models/modbridge_v2/corridors/size3/4way.mdl",
+    "models/modbridge_v2/corridors/size3/deadend.mdl",
+    "models/modbridge_v2/corridors/size3/glassend.mdl",
+    "models/modbridge_v2/corridors/size3/glass_2p.mdl",
+    "models/modbridge_v2/corridors/size3/glass_1p.mdl",
+    "models/modbridge_v2/corridors/size3/glass_0p1.mdl",
+    "models/modbridge_v2/corridors/size3/glass_0p2.mdl",
+    "models/modbridge_v2/corridors/size3/glass_0p3.mdl",
+    "models/modbridge_v2/corridors/size3/straight1_sw.mdl",
+    "models/modbridge_v2/corridors/size3/straight2_sw.mdl",
+    "models/modbridge_v2/corridors/size3/straight3_sw.mdl",
+}
+
+local size_3x2 = {
+    "models/modbridge_v2/corridors/size3x2/straight1.mdl",
+    "models/modbridge_v2/corridors/size3x2/straight2.mdl",
+    "models/modbridge_v2/corridors/size3x2/straight3.mdl",
+    "models/modbridge_v2/corridors/size3x2/turn.mdl",
+    "models/modbridge_v2/corridors/size3x2/3way.mdl",
+    "models/modbridge_v2/corridors/size3x2/4way.mdl",
+    "models/modbridge_v2/corridors/size3x2/deadend.mdl",
+    "models/modbridge_v2/corridors/size3x2/glassend.mdl",
+}
+
+local bridges = {
+    "models/modbridge_v2/corridors/bridges/triangle_s.mdl",
+    "models/modbridge_v2/corridors/bridges/triangle_sl.mdl",
+    "models/modbridge_v2/corridors/bridges/round_s.mdl",
+    "models/modbridge_v2/corridors/bridges/triangle_sq.mdl",
+}		
+
+local splitters = {
+    "models/modbridge_v2/corridors/splitters/c3_1.mdl",
+    "models/modbridge_v2/corridors/splitters/c2_1.mdl",
+    "models/modbridge_v2/corridors/splitters/c2_3.mdl",
+    "models/modbridge_v2/corridors/splitters/s1_2.mdl",
+    "models/modbridge_v2/corridors/splitters/s1_3.mdl",
+    "models/modbridge_v2/corridors/splitters/s2_3.mdl",
+    "models/modbridge_v2/corridors/splitters/f1_3.mdl",
+    "models/modbridge_v2/corridors/splitters/s3_32.mdl",
+    "models/modbridge_v2/corridors/splitters/c3_1l.mdl",
+    "models/modbridge_v2/corridors/splitters/c3_1r.mdl",
+    "models/modbridge_v2/corridors/splitters/c3_1c.mdl",
+    "models/modbridge_v2/corridors/splitters/c1_32.mdl",
+    "models/modbridge_v2/corridors/splitters/c2_32.mdl",
+    "models/modbridge_v2/corridors/splitters/c3_32.mdl",
+}
+
+local height_transfers = {
+    "models/modbridge_v2/corridors/height_transfers/s1.mdl",
+    "models/modbridge_v2/corridors/height_transfers/s2.mdl",
+    "models/modbridge_v2/corridors/height_transfers/s3.mdl",
+    "models/modbridge_v2/corridors/height_transfers/sp3_1.mdl",
+    "models/modbridge_v2/corridors/height_transfers/sp3_2.mdl",
+}
+
+local miscellaneous = {
+    "models/modbridge_v2/corridors/misc/pillar.mdl",
+    "models/modbridge_v2/corridors/misc/pillar_double.mdl",
+    "models/modbridge_v2/corridors/misc/wall.mdl",
+}
+
+hook.Add( "PopulatePropMenu", "Modbridge V2", function()
+    local contents = {}
+
+    table.insert( contents, {
+        type = "header",
+        text = "Size 1 Corridors"
+    })
+
+    for i = 1,#size_1 do
+        table.insert( contents, {
+            type = "model",
+            model = size_1[i]
+        })
+    end
+
+    table.insert( contents, {
+        type = "header",
+        text = "Size 2 Corridors"
+    })
+
+    for i = 1,#size_2 do
+        table.insert( contents, {
+            type = "model",
+            model = size_2[i]
+        })
+    end
+
+    table.insert( contents, {
+        type = "header",
+        text = "Size 3 Corridors"
+    })
+
+    for i = 1,#size_3 do
+        table.insert( contents, {
+            type = "model",
+            model = size_3[i]
+        })
+    end
+
+    table.insert( contents, {
+        type = "header",
+        text = "Size 3 tall Corridors"
+    })
+
+    for i = 1,#size_3x2 do
+        table.insert( contents, {
+        type = "model",
+        model = size_3x2[i]
+        })
+    end
+
+    table.insert( contents, {
+        type = "header",
+        text = "Bridges"
+    })
+
+    for i = 1,#bridges do
+        table.insert( contents, {
+        type = "model",
+        model = bridges[i]
+        })
+    end
+
+
+    table.insert( contents, {
+        type = "header",
+        text = "Splitters"
+    })
+
+    for i = 1,#splitters do
+        table.insert( contents, {
+            type = "model",
+            model = splitters[i]
+        })
+    end
+
+    table.insert( contents, {
+        type = "header",
+        text = "Height Transfers"
+    })
+
+    for i = 1,#height_transfers do
+        table.insert( contents, {
+            type = "model",
+            model = height_transfers[i]
+        })
+    end
+
+    table.insert( contents, {
+        type = "header",
+        text = "Miscellaneous"
+    })
+
+    for i = 1,#miscellaneous do
+        table.insert( contents, {
+            type = "model",
+            model = miscellaneous[i]
+        })
+    end
+
+    spawnmenu.AddPropCategory( "modbridge_v2", "Modbridge V2", contents, "" )
+end)
